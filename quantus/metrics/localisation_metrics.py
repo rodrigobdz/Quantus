eaea526c14ba3be1fe1c3e859576ab0725d97caf
Kristoffer Wickstrøm <Wickstrom.kristoffer@gmail.com>
2022-02-14 17:32:55 +0100

    Update localisation_metrics.py
    Fixed typo (line 132).

--- quantus/metrics/localisation_metrics.py
+++ quantus/metrics/localisation_metrics.py
@@ -129,7 +129,7 @@ class PointingGame(Metric):
 
             # Initialise the metric and evaluate explanations by calling the metric instance.
             >> metric = PointingGame(abs=True, normalise=False)
-            >> scores = metric(model=model, x_batch=x_batch, y_batch=y_batch, a_batch=a_batch_saliency, **{}}
+            >> scores = metric(model=model, x_batch=x_batch, y_batch=y_batch, a_batch=a_batch_saliency, **{})
         """
         # Reshape input batch to channel first order:
         self.channel_first = kwargs.get("channel_first", get_channel_first(x_batch))
